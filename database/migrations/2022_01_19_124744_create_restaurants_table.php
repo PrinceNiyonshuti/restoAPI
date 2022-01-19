6d5c1f7e3c042136b2eb5bc73c2655f7062ce4c4
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-19 14:58:15 +0200

    created restaurant model and controller

---
+++ database/migrations/2022_01_19_124744_create_restaurants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sector_id');
+            $table->text('restoName');
+            $table->integer('rating');
+            $table->string('location');
+            $table->string('thumbnail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
