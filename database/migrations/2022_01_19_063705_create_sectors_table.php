c3e742f05825115e69f68d9ec6126d3fd9489350
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-19 08:44:09 +0200

    created sector model and migration

---
+++ database/migrations/2022_01_19_063705_create_sectors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sectors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('district_id');
+            $table->string('sectorName')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sectors');
+    }
+}
